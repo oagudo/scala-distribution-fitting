6303fd7475187f3e096bacb455ed59a6e815429c
oagudo <omar.agudo@gmail.com>
2015-05-09 09:19:57 +0200

    Cdf and invCdf methods for NormalDistribution implemented

--- src/main/scala/model/distribution/NormalDistribution.scala
+++ src/main/scala/model/distribution/NormalDistribution.scala
@@ -6,8 +6,10 @@ case class NormalDistribution(mu: Double, sigma: Double) extends Distribution[Do
 
   require(sigma > 0)
 
-  def cdf(x: Double) : Double = throw new NotImplementedException()
-  def invCdf(y: Double) : Double = throw new NotImplementedException()
+  private val impl = new breeze.stats.distributions.Gaussian(mu, sigma)
+
+  def cdf(x: Double) : Double = impl.cdf(x)
+  def invCdf(y: Double) : Double = impl.icdf(y)
   def pdf(x: Double) : Double = throw new NotImplementedException()
   def random() : Double = throw new NotImplementedException()
 }
